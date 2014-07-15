2aafa322b7b48cff2e1a43290d3204a7475d9fcf
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2014-07-15 13:26:58 +0900

    add description

--- ofp4/README.go
+++ ofp4/README.go
@@ -1,6 +1,8 @@
 /*
 Package ofp4 implements openflow 1.3 protocol structures.
 
+ofp4: ofp is short for openflow protocol, and 4 is "Protocol version 0x04".
+
 ofp4 structs implement encoding.BinaryMarshaler and encoding.BinaryUnmarshaler
 interfaces.
 
