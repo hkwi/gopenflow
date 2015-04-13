ee16b292a4bd319b97f0239dc9ee1f22e533a2e4
Hiroaki KAWAI <hiroaki.kawai@gmail.com>
2015-04-13 15:06:09 +0900

    update the doc

--- ofp4/README.go
+++ ofp4/README.go
@@ -3,12 +3,6 @@ Package ofp4 implements openflow 1.3 protocol structures.
 
 ofp4: ofp is short for openflow protocol, and 4 is "Protocol version 0x04".
 
-ofp4 structs implement encoding.BinaryMarshaler and encoding.BinaryUnmarshaler
-interfaces.
-
-Examples:
- var message ofp4.Message
- err := message.UnmarshalBinary(data []byte)
- fmt.Println(message.Type) // ofp_header.type
+ofp4 types are useful for message encoding, decoding.
 */
 package ofp4
